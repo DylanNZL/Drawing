7aeda317f839dcaf16aeeab05b3a525ef25b7dac
Dylan Cross <dylancross@me.com>
2016-08-16 11:09:10 +1200

    Change storage to a single instance to be housed in a list of storage classes.

--- src/Storage.java
+++ src/Storage.java
@@ -1,22 +1,31 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 /**
  * Created by Dylan on 14-Aug-16.
  */
 public class Storage {
     // Starting X & Y co-ordinates
-    ArrayList<Integer> startX = new ArrayList<>();
-    ArrayList<Integer> startY = new ArrayList<>();
+    public int startX;
+    public int startY;
     // Finishing X & Y co-ordinates
-    ArrayList<Integer> finishX = new ArrayList<>();
-    ArrayList<Integer> finishY = new ArrayList<>();
+    public int finishX;
+    public int finishY;
     // Color
-    ArrayList<Color> color = new ArrayList<>();
+    public Color myColor;
     // Fill
-    ArrayList<Boolean> fill = new ArrayList<>();
+    public Boolean fill;
     // Shape
-    ArrayList<ControlPanel.MyShape> shape = new ArrayList<>();
-    // ID
-    int ids;
+    ControlPanel.MyShape shape;
+
+    public Storage(int m_x1, int m_x2, int m_y1, int m_y2, Color m_color, Boolean m_fill, ControlPanel.MyShape m_shape) {
+        startX = m_x1;
+        startY = m_y1;
+        finishX = m_x2;
+        finishY = m_y2;
+        myColor = m_color;
+        fill = m_fill;
+        shape = m_shape;
+    }
 }
