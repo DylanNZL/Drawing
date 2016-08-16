e2b1a239c087b5c71f72a711be5eb42a192f3d19
Dylan Cross <dylnnzl@gmail.com>
2016-08-16 20:11:27 +1200

    Added setter functions + functions to return X2-X1 and Y1-Y2

--- src/Storage.java
+++ src/Storage.java
@@ -19,6 +19,10 @@ public class Storage {
     // Text (might be blank)
     private String m_Text;
 
+    public Storage() {
+
+    }
+
     public Storage(int m_x1, int m_x2, int m_y1, int m_y2, Color m_color, Boolean m_fill, ControlPanel.MyShape m_shape) {
         // Constructor for non-text shapes
         m_X1 = m_x1;
@@ -54,6 +58,15 @@ public class Storage {
         m_Y1 = m_Y2;
         m_Y2 = temp;
     }
+    // Get the length of a shape
+    public int length() {
+        return m_X2 - m_X1;
+    }
+    // Get the height of a shape
+    public int height() {
+        return m_Y2 - m_Y1;
+    }
+
     // Get functions
     public int getX1 () { return m_X1; }
     public int getY1 () { return m_Y1; }
@@ -63,4 +76,13 @@ public class Storage {
     public Boolean getFill () { return m_Fill; }
     public ControlPanel.MyShape getShape () { return m_Shape; }
     public String getText () { return m_Text; }
+    // Set functions
+    public void setX1 (int newX) { m_X1 = newX; }
+    public void setY1 (int newY) { m_Y1 = newY; }
+    public void setX2 (int newX) { m_X2 = newX; }
+    public void setY2 (int newY) { m_Y2 = newY; }
+    public void setMyColor (Color newColor) { m_MyColor = newColor; }
+    public void setFill (Boolean newFill) { m_Fill = newFill; }
+    public void setShape (ControlPanel.MyShape newShape) { m_Shape = newShape; }
+    public void setText (String newText) { m_Text = newText; }
 }
