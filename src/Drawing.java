524b51637f4308e3c56bd2641f52491ecab534cf
Dylan Cross <dylancross@me.com>
2016-08-16 13:43:05 +1200

    Removed menu class (directly implemented it into Drawing.java) and tidied up some imports

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
